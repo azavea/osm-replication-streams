90f65172b03f606f4be66295975f9e2883096e53
CloudNiner <andy@finkmiller.com>
2020-05-18 14:57:34 -0400

    Add AugmentedDiff parser tests
    - Count objects in parsed results
    - Verify correct structure for parsed nodes
      (created + modified + deleted)

---
+++ test/expectations.js
@@ -0,0 +1,114 @@
+module.exports = {
+  nodeCreated: {
+    old: null,
+    new: {
+      geometry: {
+        coordinates: [14.6484835, 48.143357],
+        type: "Point",
+      },
+      id: "new",
+      properties: {
+        changeset: "83957982",
+        id: "7434944955",
+        tags: {},
+        timestamp: "2020-04-22T19:03:08Z",
+        type: "node",
+        uid: "387904",
+        user: "flash3",
+        version: "1",
+        visible: true,
+      },
+      type: "Feature",
+    },
+  },
+  nodeModified: {
+    old: {
+      geometry: {
+        coordinates: [-16.5921061, 13.2320036],
+        type: "Point",
+      },
+      id: "old",
+      properties: {
+        changeset: "68624648",
+        id: "360444888",
+        tags: {},
+        timestamp: "2019-03-28T12:55:47Z",
+        type: "node",
+        uid: "7725447",
+        user: "ChadSi",
+        version: "5",
+        visible: true,
+      },
+      type: "Feature",
+    },
+    new: {
+      geometry: {
+        coordinates: [-16.5919425, 13.2305832],
+        type: "Point",
+      },
+      id: "new",
+      properties: {
+        changeset: "83958001",
+        id: "360444888",
+        tags: {},
+        timestamp: "2020-04-22T19:03:51Z",
+        type: "node",
+        uid: "9461095",
+        user: "yellow_car47",
+        version: "6",
+        visible: true,
+      },
+      type: "Feature",
+    },
+  },
+  nodeDeleted: {
+    old: {
+      geometry: {
+        coordinates: [-0.4983433, 43.8887198],
+        type: "Point",
+      },
+      id: "old",
+      properties: {
+        changeset: "7584727",
+        id: "941243822",
+        tags: {
+          amenity: "bicycle_parking",
+          capacity: "2",
+          source:
+            "cadastre-dgi-fr source : Direction Générale des Impôts - Cadastre. Mise à jour : 2010",
+        },
+        timestamp: "2011-03-17T12:11:11Z",
+        type: "node",
+        uid: "196035",
+        user: "vendangeur",
+        version: "2",
+        visible: true,
+      },
+      type: "Feature",
+    },
+    new: {
+      geometry: {
+        coordinates: [-0.4983433, 43.8887198],
+        type: "Point",
+      },
+      id: "new",
+      properties: {
+        changeset: "83958004",
+        id: "941243822",
+        tags: {
+          amenity: "bicycle_parking",
+          capacity: "2",
+          source:
+            "cadastre-dgi-fr source : Direction Générale des Impôts - Cadastre. Mise à jour : 2010",
+        },
+        timestamp: "2020-04-22T19:03:53Z",
+        type: "node",
+        uid: "10917218",
+        user: "ERIC JAOUEN",
+        version: "3",
+        visible: false,
+      },
+      type: "Feature",
+    },
+  },
+};
